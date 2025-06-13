68f3d07c58714efeaf7b9675fdca434624716bb0
Integer003 <1204649252@qq.com>
2025-06-13 15:44:33 +0800

    Change basic style

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,8 +26,8 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://fuwari.vercel.app/",
-	base: "/",
+	site: "https://Integer003.github.io",
+	base: "PaperSharing",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
